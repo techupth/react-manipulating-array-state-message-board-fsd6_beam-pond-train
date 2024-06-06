197f90756873f643eb848a7795cb00cf42cf5589
trainthunder <kitikun.po@gmail.com>
2024-06-06 13:10:05 +0700

    feat: TrainHackhour

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [inputMessage, setInputMessage] = useState("");
+  const [listMessage, setListMessage] = useState([]);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    const newMessage = [...listMessage];
+    newMessage.push(inputMessage);
+    setListMessage(newMessage);
+    setInputMessage("");
+  };
+
+  const handleDelete = (indexMessage) => {
+    const deleteMessage = [...listMessage];
+    deleteMessage.splice(indexMessage, 1);
+    setListMessage(deleteMessage);
+  };
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -9,15 +28,32 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={inputMessage}
+            onChange={(event) => {
+              setInputMessage(event.target.value);
+            }}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={handleClick}>
+          Submit
+        </button>
       </div>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        {listMessage.map((items, index) => {
+          return (
+            <div className="message" key={index}>
+              <h1>{items}</h1>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  handleDelete(index);
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
